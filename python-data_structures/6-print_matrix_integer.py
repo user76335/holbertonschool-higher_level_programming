6b74d378c90283f6b794287403a1d9652d4f7e12
user76335 <11958@holbertonstudents.com>
2025-12-06 16:31:56 +0000

    matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            end = " " if i < len(row) - 1 else ""
+            print("{:d}".format(row[i]), end=end)
+        print()
