1968d5f875e73390dec76c88603964f37f9abc32
user76335 <11958@holbertonstudents.com>
2025-12-20 17:15:25 +0000

    inherits and goemetey

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines a class-inheritance checking function.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
