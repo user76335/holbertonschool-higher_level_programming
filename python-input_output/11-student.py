e51857f0c554dcef0b6e87cb7cbe193646e2982b
user76335 <11958@holbertonstudents.com>
2025-12-26 14:55:30 +0000

    student and trangle

---
+++ python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Student class module"""
+
+
+class Student:
+    """Defines a Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return the dictionary representation.
+        If attrs is a list, only selected attributes are returned.
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace attributes of the instance from a dictionary"""
+        for key, value in json.items():
+            setattr(self, key, value)
