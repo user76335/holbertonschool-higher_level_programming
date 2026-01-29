ad5641a22c79a6fa56c6135cfbbad70f4296b107
user76335 <11958@holbertonstudents.com>
2026-01-29 11:21:12 +0000

    no genre count shows and my genres

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked --
+SELECT ts.title, tg.genre_id
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tg
+ON ts.id = tg.show_id
+WHERE tg.genre_id IS NULL
+ORDER BY ts.title ASC, tg.genre_id ASC;
